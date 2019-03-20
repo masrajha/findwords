b89f462babcb2bc03630a427553c5adba30c8da8
Didik Kurniawan <didikunila@gmail.com>
2019-03-20 19:41:15 +0700

    add printDiag test print diagonal only

--- main.cpp
+++ main.cpp
@@ -2,48 +2,73 @@
 #include <cstring>
 
 using namespace std;
-const int cols=16, rows=15;
+const int cols = 16, rows = 15;
 
-const char words [rows][cols] = { "tgbwwinterwsesn", 
-                            "aaunttmmhfoodnb", 
-                            "jlwcqldzmpmvdmr", 
-                            "asagmquwvvbsohi", 
-                            "bwplotanadtpgnc", 
-                            "rewngodjcpnatnk", 
-                            "eeotwosbqharrsa", 
-                            "azcgeswewnaknpb", 
-                            "dinnerqodlwdcar", 
-                            "onopkwmparktzcc", 
-                            "qbfrogmamwpweey", 
-                            "lqzqnnmrzjjsclg", 
-                            "mosgzczetdbooto", 
-                            "pdcrzmsngrdnrpz", 
-                            "ohnkzwaterjgtra"};
+const char words[rows][cols] = {"tgbwwinterwsesn",
+                                "aaunttmmhfoodnb",
+                                "jlwcqldzmpmvdmr",
+                                "asagmquwvvbsohi",
+                                "bwplotanadtpgnc",
+                                "rewngodjcpnatnk",
+                                "eeotwosbqharrsa",
+                                "azcgeswewnaknpb",
+                                "dinnerqodlwdcar",
+                                "onopkwmparktzcc",
+                                "qbfrogmamwpweey",
+                                "lqzqnnmrzjjsclg",
+                                "mosgzczetdbooto",
+                                "pdcrzmsngrdnrpz",
+                                "ohnkzwaterjgtra"};
 
 char *getWordVertical(int);
-char* strrev( char*);
+char *strrev(char *);
 bool searchVertical(char *);
+void printDiag(int);
 
 int main()
 {
-    char word[]="zogy";
+    char word[] = "zogy";
     if (searchVertical(word))
-        cout<<"Ada";
-        else cout<<"Tidak Ada";
+        cout << "Ada";
+    else
+        cout << "Tidak Ada";
+    cout << endl;
+    printDiag(0);
     return 0;
 }
+
+void printDiag(int col)
+{
+    int i = col;
+
+    while (i < cols - 1)
+    {
+        int k = i;
+        int l = 0;
+        while (k < cols -1 && l < rows)
+        {
+            cout << words[k][l];
+            k++;
+            l++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
 /**
  * Fungsi untuk mencari kata secara vertikal dari atas dan dari bawah
- */ 
-bool searchVertical(char *word){
+ */
+bool searchVertical(char *word)
+{
     char *s;
-    for (int i=0;i<cols-1;i++){
+    for (int i = 0; i < cols - 1; i++)
+    {
         s = getWordVertical(i);
-        if (strstr(s,word)>0)
+        if (strstr(s, word) > 0)
             return true;
-        if (strstr(strrev(s),word)>0)
+        if (strstr(strrev(s), word) > 0)
             return true;
-        
     }
     return false;
 }
@@ -51,35 +76,38 @@ bool searchVertical(char *word){
 /** 
  * Fungsi untuk mengambil string dari kolom ke i 
  */
-char *getWordVertical(int i){
-    char *ret=new char;
+char *getWordVertical(int i)
+{
+    char *ret = new char;
 
-    for (int j=0;j<cols-1;j++){
-            *(ret+j)=words[j][i];
-        }
-    *(ret+(cols-1))='\0';
+    for (int j = 0; j < cols - 1; j++)
+    {
+        *(ret + j) = words[j][i];
+    }
+    *(ret + (cols - 1)) = '\0';
     return ret;
 }
 
 /** Fungsi untuk membalik kata
     code from http://www.cplusplus.com/forum/general/14951/
 */
-char* strrev( char* s )
-  {
-  char  c;
-  char* s0 = s - 1;
-  char* s1 = s;
+char *strrev(char *s)
+{
+    char c;
+    char *s0 = s - 1;
+    char *s1 = s;
 
-  /* Find the end of the string */
-  while (*s1) ++s1;
+    /* Find the end of the string */
+    while (*s1)
+        ++s1;
 
-  /* Reverse it */
-  while (s1-- > ++s0)
+    /* Reverse it */
+    while (s1-- > ++s0)
     {
-    c   = *s0;
-    *s0 = *s1;
-    *s1 =  c;
+        c = *s0;
+        *s0 = *s1;
+        *s1 = c;
     }
 
-  return s;
-  }
+    return s;
+}
